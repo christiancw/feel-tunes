bb16d5f7f703d56fb82b297137a8752b4afc5472
Christian Wentling <christianwentling@gmail.com>
2017-06-17 13:20:45 -0400

    can specify multiple genres assigned to feelings

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ Turning feelings into music.
 
 
 * `npm install`, or `yarn install` - whatever you're into
-* Create two postgres databases: `feeltunes` and `feeltunes-test`
+* Create two postgres databases: `feeltunes` and `feeltunes-test` (right now not in use)
   * By default, running `npm test` will use `feeltunes-test`, while regular development uses `feeltunes`
 
 
 ## Start
 
-`npm start` will make great things happen!
+`npm start`
 
 If you want to run the server and/or webpack separately, you can also `npm run start-server` and `npm run build-client`.
--- server/api/playlist.js
+++ server/api/playlist.js
@@ -4,7 +4,7 @@ module.exports = router;
 const clientId = process.env.SPOTIFY_CLIENT_ID;
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 //  requests authorization
-var authOptions = {
+const authOptions = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
     'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
@@ -18,14 +18,22 @@ var authOptions = {
 //setting artists, tracks, generes as seeds for the recommendation request
 
 // const currentMood = 'tired';
-
+const happyGenres = ['chicago-house', 'edm', 'funk', 'latin', 'pop'].join('&');
+const sadGenres = ['acoustic', 'folk', 'sad', 'soul', 'grunge'].join('&');
 const emotionLookup = {
-  happy: 1,
-  sad: 2,
+  happy: happyGenres,
+  sad: sadGenres,
   energetic: 3,
-  tired: 4
+  tired: 4,
+  afraid: 5,
+  disgusted: 6,
+  angry: 7,
+  surprised: 8
 };
 
+
+
+// [ "acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music" ]
 const genres = [
       'alt_rock',
       'bluegrass',
@@ -50,14 +58,14 @@ const trackSeed = '0c6xIDDpzE81m2q797ordA';
 router.get('/', (req, res, next) => {
   console.log('REQBODY==>', req.query.id)
   const currentMood = req.query.id;
-  const genreSeed = genres[emotionLookup[currentMood]];
+  const genreSeed = emotionLookup[currentMood];
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
 
       // use the access token to access the Spotify Web API
       const token = body.access_token;
       var options = {
-        url: `https://api.spotify.com/v1/recommendations?seed_genres=${genreSeed}&min_energy=0.4&min_popularity=50&market=US`,
+        url: `https://api.spotify.com/v1/recommendations?seed_genres=${genreSeed}&market=US`,
         headers: {
           'Authorization': 'Bearer ' + token
         },

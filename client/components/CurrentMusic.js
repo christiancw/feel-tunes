3221b0704fb5dce0fa3b57737ff25a36bb4fc584
Christian Wentling <christianwentling@gmail.com>
2017-10-02 12:38:51 -0400

    makes tracks list respond to screen size

--- client/components/CurrentMusic.js
+++ client/components/CurrentMusic.js
@@ -4,6 +4,17 @@ export default function CurrentMusic (props) {
   const tracks = props.currentMusic;
   const handleClearButton = props.handleClearButton;
   const fetchingMusic = props.fetchingMusic;
+  const setColumn = num => {
+    if (num % 3 === 0){
+      return 'col-left';
+    }
+    if (num % 3 === 1){
+      return 'col-middle';
+    }
+    if (num % 3 === 2){
+      return 'col-right';
+    }
+  };
 
   return (
     <div className="track-response">
@@ -15,20 +26,21 @@ export default function CurrentMusic (props) {
       {fetchingMusic ?
       <div className="loader"/>
       :
-      null
-      }
       <ul>
-        {tracks && tracks.map(track => {
-          return (
-            <li
-              key={track.id}>
-              <div>
-                <iframe src={`https://open.spotify.com/embed?uri=spotify%3Atrack%3A${track.id}`} width="300" height="380" frameBorder="0" allowTransparency="true"></iframe>
-              </div>
-            </li>
-          );
-        })}
+          {tracks && tracks.map((track, index) => {
+            return (
+              <li
+                key={track.id}
+                className={setColumn(index)}
+                >
+                <div>
+                  <iframe src={`https://open.spotify.com/embed?uri=spotify%3Atrack%3A${track.id}`} width="300" height="380" frameBorder="0" allowTransparency="true"></iframe>
+                </div>
+              </li>
+            );
+          })}
       </ul>
+      }
     </div>
   );
 }
